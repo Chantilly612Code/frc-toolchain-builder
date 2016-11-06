00a07301e5ee75d82ffda209b1f65eac9ea753bb
toastertaster <toastertaster@gmail.com>
2016-11-06 09:12:37 -0500

    Updates to the repository to make the build system work with Ubuntu 16.10.  Added install-dependendies.sh to install necessary developer build utilities and libraries.
    versions.sh is updated to reflect newer files available on the NI webpage.
    
    changelog files are updated to reflect new versions and to correct an issue with the month no longer being parseable by debuild (Sept->Sep and it works fine)

---
+++ install-dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# these are necessary to successfully build the ubuntu FRC toolchain
+sudo apt-get install docbook-xsl docbook-xml xsltproc  libbz2-dev libmpc-dev libcloog-isl-dev dejagnu quilt chrpath devscripts zsh
+
+# unsure if this is necessary
+sudo apt-get install binutils-arm-frc-linux-gnueabi 
+
+
+
